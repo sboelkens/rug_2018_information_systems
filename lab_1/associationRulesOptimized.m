a52d02d398b0fcf127c6b4d9ebc1fe81629d380c
Siemen Boelkens <s.r.boelkens@student.rug.nl>
2018-11-16 14:51:43 +0100

    Added optimization, but still not faster than the original

--- lab_1/associationRulesOptimized.m
+++ lab_1/associationRulesOptimized.m
@@ -2,7 +2,7 @@
 % Association Rule Analysis with Apriori
 % Author: Dr. George Azzopardi
 % Date: November 2018
-function [AR,confidence,support] = associationRules(minsup,minconf)
+function [AR,confidence,support] = associationRulesOptimized(minsup,minconf)
 
 if nargin == 0
     minsup = 0.001;
@@ -97,25 +97,39 @@ sup = [];
 % We consider the itemsets that have at least two items
 for k = 2:numel(frequentItems)
     for j = 1:size(frequentItems{k},1)
-        % Improve the following code by to make the consideration of
-        % association rules much more efficient
-        for i = 1:numel(frequentItems{k}(j,:))-1
+        lowConfList = {};
+        % Improvement: Turn around foreach loop to start with the most
+        % antecedents, then go lower instead of other way around
+        for i = numel(frequentItems{k}(j,:))-1:-1:1
             antecendentlist = nchoosek(frequentItems{k}(j,:),i);
-            for l = 1:size(antecendentlist,1)                
+            for l = 1:size(antecendentlist,1)       
                 consequence = find(~ismember(frequentItems{k}(j,:),antecendentlist(l,:)));
-                confidence = support{k}(j)*ntrans/sum(all(dataset(:,antecendentlist(l,:)),2));
-                if confidence >= minconf
-                    sup(end+1) = support{k}(j);
-                    conf(end+1) = confidence;
-                    antecedentstr = '';
-                    for m = 1:size(antecendentlist,2)
-                        antecedentstr = [antecedentstr,trlbl{antecendentlist(l,m)},','];
+                isSubsetOf = false;
+                % Improvement: check if our current antecedentlist member
+                % is a subset of what is already in the lowConfList
+                for x = 1:size(lowConfList)
+                    isSubsetOf = all(ismember(antecendentlist(l,:), lowConfList{x}));
+                end
+                if isSubsetOf == false
+                    confidence = support{k}(j)*ntrans/sum(all(dataset(:,antecendentlist(l,:)),2));
+                    if confidence >= minconf
+                        sup(end+1) = support{k}(j);
+                        conf(end+1) = confidence;
+                        antecedentstr = '';
+                        for m = 1:size(antecendentlist,2)
+                            antecedentstr = [antecedentstr,trlbl{antecendentlist(l,m)},','];
+                        end
+                        consequencestr = '';
+                        for m = 1:numel(consequence)
+                            consequencestr = [consequencestr,trlbl{frequentItems{k}(j,consequence(m))},','];
+                        end
+                        AR{end+1} = [antecedentstr(1:end-1),' -> ',consequencestr(1:end-1),sprintf(' [%2.2f,%2.2f]',sup(end),conf(end))];
+                    else
+                        % Improvement: if confidence is not high enough, store
+                        % it in the lowConfList to keep track of antecendents
+                        % that do not have high enough confidence
+                        lowConfList{end+1} = antecendentlist(l,:);
                     end
-                    consequencestr = '';
-                    for m = 1:numel(consequence)
-                        consequencestr = [consequencestr,trlbl{frequentItems{k}(j,consequence(m))},','];
-                    end
-                    AR{end+1} = [antecedentstr(1:end-1),' -> ',consequencestr(1:end-1),sprintf(' [%2.2f,%2.2f]',sup(end),conf(end))];
                 end
             end
         end        
